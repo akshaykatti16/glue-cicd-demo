54c34167909ff95234ac8f9fef4040eeb2ecde8d
Akshay Katti <akshay.katti16@gmail.com>
2026-01-21 11:48:48 +0530

    updated message variable

---
+++ envs/dev/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "glue_job" {
+  source          = "../../modules/glue-job"
+  job_name        = "hello-glue-dev"
+  role_arn        = var.role_arn
+  script_location = var.script_location
+  env_name        = "DEV"
+  message         = "Hola from DEV environment"
+}
